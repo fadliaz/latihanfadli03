00c7318fba9e5f0336368727585ff1794fabb3e1
fadli11 <frosyadi20@gmail.com>
2019-02-23 17:12:43 +0700

    nambah file

---
+++ Latihan02.py
@@ -0,0 +1,9 @@
+print("Masukan Bilangan Random,dan berhenti dalam angka 0")
+
+b= 0
+while True:
+        angka = int(input("Masukan Bilangan : "))
+        b = angka
+        if b == 0:
+                break
+print('-----selesai-----')
